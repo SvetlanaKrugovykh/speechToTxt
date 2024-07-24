2f7e8c0162a2dc7f7058f42eaf48d8cc3144af43
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-07-24 22:54:21 +0200

    some improvments

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, jsonify
 import os
 import sys
+import urllib3
 import requests
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 main = Blueprint('main', __name__)
 
@@ -12,9 +15,9 @@ except ImportError:
 
 from .converters.audio_converter import convert_to_wav
 
-def generate_filename(segment_number, userId=""):
+def generate_filename(segment_number, user_id=""):
     segment_name = os.getenv('SEGMENT_NAME', 'segment')
-    return f"{userId}_{segment_name}_{segment_number}.wav"
+    return f"{user_id}_{segment_name}_{segment_number}.wav"
     
 def transcribe_audio(file_path):
     try:
@@ -28,19 +31,34 @@ def transcribe_audio(file_path):
 
 @main.route('/upload', methods=['POST'])
 def upload_audio():
-    userId = ""
+    user_id = ""
     if int(os.getenv('CHECK_AUTHORIZATION', '0')) == 1:
         auth_header = request.headers.get('Authorization')
         if not auth_header:
             return jsonify({'error': 'Authorization header missing'}), 401
         
         auth_url = os.getenv('AUTH_URL')
-        auth_response = requests.post(auth_url, headers={'Authorization': auth_header, 'Content-Type': 'application/json'}, json={'token': auth_header})
-        
-        if auth_response.status_code != 200 or not auth_response.json().get('valid') or not auth_response.json().get('data'):
+        auth_response = requests.post(
+          auth_url, 
+          headers={'Authorization': auth_header, 'Content-Type': 'application/json'}, 
+          json={'token': auth_header},
+          verify=False )
+
+        print(f"Auth response status code: {auth_response.status_code}")
+        print(f"Auth response content: {auth_response.content}")
+
+        if auth_response.status_code != 200:
             return jsonify({'error': 'Authorization failed'}), 401
         
-        userId = auth_response.json().get('data').get('userId', '')
+        try:
+            auth_data = auth_response.json()
+        except ValueError:
+            return jsonify({'error': 'Invalid JSON response from authorization server'}), 500
+
+        if not isinstance(auth_data, dict) or not auth_data.get('valid') or not auth_data.get('data'):
+            return jsonify({'error': 'Authorization failed'}), 401
+        
+        user_id = auth_data.get('data').get('userId', '')
 
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
@@ -49,7 +67,7 @@ def upload_audio():
         return jsonify({'error': 'No selected file'}), 400
     if file:
         segment_number = request.form.get('segment', 'unknown')
-        filename = generate_filename(segment_number, userId)        
+        filename = generate_filename(segment_number, user_id)        
         filepath = os.path.join('uploads', filename)
         file.save(filepath)
         transcription, error = transcribe_audio(filepath)  
